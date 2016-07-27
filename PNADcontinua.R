2d5a88efe6b3a20f023b51bf47a6e51d416fec49
arturlascala <arturlascala@gmail.com>
2016-07-27 12:23:56 -0300

    Create PNADcontinua.R

---
+++ PNADcontinua.R
@@ -0,0 +1,22 @@
+# Function for reading 'PNAD contínua' data with the faster 'readr' package.
+
+# For variable 'dadosPNAD', replace with TXT file. 
+# FOr variable 'inputPNAD', replace with SAS file. 
+
+PNADcontinua <- function(dadosPNAD, inputPNAD){
+        
+        library(SAScii)  ## if not installed, use install.packages("SAScii")
+        library(readr)  ## if not installed, use install.packages("readr")
+        
+        # Create data.frame with inputs
+        dic <- parse.SAScii(inputPNAD)
+        
+        # create references for read_fwf
+        start1 <- cumsum(dic$width)-dic$width+1
+        end1 <- cumsum(dic$width)
+        dic$start <- start1
+        dic$end <- end1
+        
+        # read PNAD data
+        PNAD <<- read_fwf(dadosPNAD, fwf_positions(dic$start, dic$end, dic$varname))
+}
